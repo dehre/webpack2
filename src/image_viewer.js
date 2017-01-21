6926a7d267c36c36347ef37619fd021b1f44407f
Loris Guerra <loris4526@gmail.com>
2017-01-21 13:22:28 +0100

    add imageLoader to webpack

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,6 +1,15 @@
+import big from "../assets/big.jpg";
+import small from "../assets/small.jpg";
 import "../styles/image_viewer.css";
 
+// SMALL IMAGE
 const image = document.createElement('img');
-image.src = "http://lorempixel.com/400/400";
+image.src = small;
 
 document.body.appendChild(image);
+
+// BIG IMAGE
+const bigImage = document.createElement('img');
+bigImage.src = big;
+
+document.body.appendChild(bigImage);
